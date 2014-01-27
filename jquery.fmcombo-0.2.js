51b9bfe53f5b602ed8ea57495d6f886e595273de
ppx80 <fmichelucci@gmail.com>
2014-01-27 17:55:45 +0100

    Use advanced plugin authoring

--- jquery.fmcombo-0.2.js
+++ jquery.fmcombo-0.2.js
@@ -1,7 +1,7 @@
 /*
- * fmcombo - jQuery plugin 0.1pre
+ * fmcombo - jQuery plugin 0.2
  *
- * Copyright (c) 2011 Fabio Michelucci <fmichelucci@gmail.com>
+ * Copyright (c) 2014 Fabio Michelucci <fmichelucci@gmail.com>
  *
  * Dual licensed under the MIT and GPL licenses:
  *   http://www.opensource.org/licenses/mit-license.php
@@ -9,132 +9,132 @@
  *
  */	
 (function( $ ){
+  	
+    //Plugin namespace
+    $.fn.fmcombo = function(options) {
 
+        var opts = $.extend({}, $.fn.fmcombo.defaults, options);
 
-  // Plugin defaults settings added as a property on plugin function.		
-  $.fn.fmcombo.defaults = {
-      source: null,    
-	  depElements: [],
-	  nextElement: null,
-	  firstOption: { Value:'-1',Text:'-- select --'},
-	  afterLoad: function () { },
-	  onEmptySource: function () { },
-	  childs:[]  
-  };
+        return this.change(function () {
 
-  var opts = $.extend({}, $.fn.fmcombo.defaults, options); 
-  	  	   	      	   	   	
-  var $fmcombo = {
-	  
-	source: null,  				
-	
-	formatResult: function(data){
-		  var opt='<option value="'+settings.firstValue+'">'+settings.firstOption+'</option>'
-		  $.each(data,function(key,value){
-				opt += '<option value="'+data[key].Value+'">'+data[key].Text+'</option>';
-			});		
-		  return opt;
-	},
-		
-	initSource: function(obj,elements,request){	
-			
-	  	   if($.isArray(request)){
-		  	  
-		  	  $fmcombo.source = $fmcombo.formatResult(request);
-		  	   
-		   } else if (typeof request === "string"){
-			   var url = request+$fmcombo.serialize(obj,elements)
-			   
-				self.xhr = $.ajax({
-					url: url,
-					dataType: "json",
-					async: false,
-					success: function (data) {
+            _self = $(this);
+            
+            var v = _self.val();
 
-					    if (data.length > 0) {
-					        $fmcombo.source = $fmcombo.formatResult(data);
-					    }
-					    else
-					    {
-					        settings.onEmptySource.call(this);
-					    }
-					},
-					error: function(){alert('Si sono verificati errori!')}
-				});
-							   
-		   } else if ($.isFunction(settings.source))settings.source.call($)  
-	},  
-	  
-    init : function(options) {        
-			     	       		
-		if ( options ) {$.extend(settings, options );}
-		
-		$(this).change(function(){					   			
-			$sel = $(this);
-			//debug($sel.attr("name"));
-			$fmcombo.initSource($sel, options.depElements, options.source);    	    
-			$(options.nextElement).html($fmcombo.source).focus();	
-			$fmcombo.sanitazeChilds(options.childs);
-		    //$.isFunction(options.afterSelect)? options.afterSelect.call($): null	
-			settings.afterSelect.call(this);
-		});
-	},
-	
-	serialize: function(obj,elements){
-		var qs = '?'+obj.attr("name")+'='+encodeURIComponent(obj.val());	
-		if($.isArray(elements)){
-			$.each(elements, function(key,value){
-				
-				$e = $(value);
-				qs += '&'+$e.attr("name")+'='+encodeURIComponent($e.val());
-				});
-		} else if (typeof elements === "string"){
-			debug('Dep is string')
-			qs += '&'+$(elements).attr("name")+'='+encodeURIComponent($(elements).val())
-		} 
-		return qs;	
-	},
-	
-	sanitazeChilds: function(elements){
-	
-		if($.isArray(elements)){
-			$.each(elements, function(key,value){
-				
-				$(value).empty()
-				});
-		} else if (typeof elements === "string"){
-			$(elements).empty()
-		}
-		
-	}	
-  };
-  //Plugin namespace
-  $.fn.fmcombo = function(method) {
+            if (v == opts.firstOption.Value || v == null || v == "") {
 
-    // Method calling logic
-    if ( $fmcombo[method] ) {
-      return $fmcombo[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-    } else if ( typeof method === 'object' || ! method ) {	   
-      return $fmcombo.init.apply( this, arguments );      
-    } else {
-      $.error( 'Method ' +  method + ' does not exist on jQuery.fmcombo' );
-    }    
+                removeOptionsFrom(opts.nextElement);
+                removeOptionsFrom(opts.childs);
+            }
+            else {
+                if ($.isArray(opts.source)) {
+
+                    $(opts.nextElement).html($.fn.fmcombo.format(opts.firstOption, opts.source)).focus();
+
+                } else if (typeof opts.source === "string") {
+
+                    var url = opts.source + serialize(_self, opts.depElements)
+
+                    self.xhr = $.ajax({
+                        url: url,
+                        dataType: "json",
+                        async: false,
+                        success: function (data) {
+
+                            if (data.length > 0) {
+                                $(opts.nextElement).html($.fn.fmcombo.format(opts.firstOption, data)).focus();
+                                opts.onAfterDataLoad.call(this);
+                            }
+                            else {
+                                opts.onEmptySource.call(this);
+                            }
+                        },
+                        error: function () { debug('Error ajax load'); }
+                    });
+
+                } else if ($.isFunction(opts.source)) opts.source.call($)
+            }            
+        });        
+    };
+
+    // Plugin defaults settings added as a property on plugin function.		
+    $.fn.fmcombo.defaults = {
+        source: null,
+        depElements: [],
+        nextElement: null,
+        firstOption: { Value: '-1', Text: '-- select --' },
+        onAfterDataLoad: function() { },
+        onEmptySource: function () { },
+        childs: []
+    };
   
-  };
+    $.fn.fmcombo.format = function(firstOption,source){
+        var opt = '<option value="' + firstOption.Value + '">' + firstOption.Text + '</option>';
+        $.each(source,function(key,value){
+            opt += '<option value="'+source[key].Value+'">'+source[key].Text+'</option>';
+        });		
+        return opt;
+    };
   
-  $.fn.fmcombo.format = function(data){
-  	var opt='<option value="'+settings.firstValue+'">'+settings.firstOption+'</option>'
-		  $.each(data,function(key,value){
-				opt += '<option value="'+data[key].Value+'">'+data[key].Text+'</option>';
-			});		
-		  return opt;
-  };
-  
-  //private function for debugging
-  function debug(message) {
-    if (window.console && window.console.log)
-      window.console.log('fmCombo: ' + message);
-  }
+    //private function 
+    function dataLoad(obj, elements, input, callback) {
+
+        if ($.isArray(input)) {
+
+            optionsData = input;
+
+        } else if (typeof input === "string") {
+            var url = input + serialize(obj, elements)
+            self.xhr = $.ajax({
+                url: url,
+                dataType: "json",
+                async: false,
+                success: function (data) {
+
+                    if (data.length > 0) {
+                        optionsData = data;
+                    }
+                    else {
+                        callback.call(this);
+                    }
+                },
+                error: function () { debug('Error ajax load');}
+            });
+
+        } else if ($.isFunction(input)) input.call($)
+    }
+
+    function serialize(obj, elements) {
+        var qs = '?' + obj.attr("name") + '=' + encodeURIComponent(obj.val());
+        if ($.isArray(elements)) {
+            $.each(elements, function (key, value) {
+
+                $e = $(value);
+                qs += '&' + $e.attr("name") + '=' + encodeURIComponent($e.val());
+            });
+        } else if (typeof elements === "string") {
+            debug('Dep is string')
+            qs += '&' + $(elements).attr("name") + '=' + encodeURIComponent($(elements).val())
+        }
+        return qs;
+    }
+
+    function removeOptionsFrom(elements) {
+
+        if ($.isArray(elements)) {
+            $.each(elements, function (key, value) {
+
+                $(value).empty()
+            });
+        } else if (typeof elements === "string") {
+            $(elements).empty()
+        }
+    }
+
+    function debug(message) {
+        if (window.console && window.console.log)
+            window.console.log('fmCombo: ' + message);
+    }
   
 
 })( jQuery );
